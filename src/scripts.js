a509b948af6350295e39617c9f405400db6e055e
Den <denya.dery@yandex.ru>
2021-05-14 15:56:00 +0300

    add code

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,16 @@
 import $ from 'jquery';
 import 'jquery-mask-plugin';
 import Swiper from 'swiper';
+import StickySidebar from 'sticky-sidebar';
 
 window.$ = $;
 window.jQuery = $;
 
+// $(() => {
+//     new StickySidebar('.block-category .col-lg-3', {
+//         containerSelector: '.block-category .container .row',
+//         innerWrapperSelector: '.block-category__sidebar_inner',
+//         bottomSpacing: 20,
+//         topSpacing: 20
+//     });
+// });
